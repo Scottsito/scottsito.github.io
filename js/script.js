c7ae0b05955cc24459432850f9961cea613d6bb2
Rowen <thatcooldudenamedrowen@gmail.com>
2025-07-26 14:07:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 
 let particles = [];
-for (let i = 0; i < 60; i++) {
+for (let i = 0; i < 250; i++) {
   particles.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
